e306ef98ca2b19830e7652e5e4198f7061fcee97
Javier nc. <ja.niloc@duocuc.cl>
2026-04-26 20:16:20 -0400

    add de seccion de noticias

--- src/components/sections/Noticias.jsx
+++ src/components/sections/Noticias.jsx
@@ -1,13 +1,37 @@
 // src/components/sections/Noticias.jsx
-
 import React from "react";
+import useHora from "../../hooks/useHora";
+import "../../styles/sections/noticias.css";
 
 function Noticias() {
+  const hora = useHora();
+
   return (
-    <div className="noticias">
-      <h2>Noticias</h2>
-      <p>Contenido de las noticias</p>
-    </div>
+    <section className="seccion-noticias">
+      <div className="contenedor-noticias">
+        <h2 className="titulo-seccion">Noticias del día</h2>
+
+        {/* info en tiempo real */}
+        <div className="info-tiempo">
+          <div className="hora">🕒 {hora}</div>
+
+          
+        </div>
+
+        {/* noticias */}
+        <div className="lista-noticias">
+          <div className="noticia">
+            <h3>Inicio de semestre</h3>
+            <p>Se dio inicio al nuevo periodo académico.</p>
+          </div>
+
+          <div className="noticia">
+            <h3>Actividad deportiva</h3>
+            <p>Gran participación de estudiantes.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
