36976cdac9d41c1e6cbc33c2e1eb8dc1ad1780a9
Javier nc. <ja.niloc@duocuc.cl>
2026-05-06 21:47:14 -0400

    add rutas protegidas

---
+++ src/routes/RutaProtegida.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+function RutaProtegida({ children, rolRequerido }) {
+  const { user, perfil } = useAuth();
+
+  if (!user) return <Navigate to="/login" />;
+  
+  if (rolRequerido && perfil?.rol !== rolRequerido) {
+    return <Navigate to="/no-autorizado" />;
+  }
+
+  return children;
+}
+
+export default RutaProtegida;
