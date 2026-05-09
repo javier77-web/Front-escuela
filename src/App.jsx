c828b7f5ee16449d22a2faa665727a4a60bca2be
Javier nc. <ja.niloc@duocuc.cl>
2026-05-09 19:53:44 -0400

    add de rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,13 @@ import CursosProfesor from "./pages/profesor/Cursos";
 import AsignaturaDetalle from "./pages/profesor/AsignaturaDetalle";
 import AnotacionesProfesor from "./pages/profesor/Anotaciones";
 import EvaluacionesProfesor from "./pages/profesor/Evaluaciones";
+import RutaProtegida from "./routes/RutaProtegida";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* PUBLICAS */}
         <Route
           path="/"
           element={
@@ -71,42 +73,152 @@ function App() {
           }
         />
 
+        {/* ALUMNO */}
         <Route
-          path="panel/alumno"
+          path="/panel/alumno"
           element={
+            <RutaProtegida rolRequerido="alumno">
               <PanelAlumno />
+            </RutaProtegida>
           }
         />
-        // rutas del panel alumno 
-        <Route path="/alumno/cursos" element={<Cursos />} />
-        <Route path="/alumno/notas" element={<Notas />} />
-        <Route path="/alumno/asistencia" element={<Asistencia />} />
-        <Route path="/alumno/anotaciones" element={<Anotaciones />} />
-        
-        <Route
-          path="panel/admin"
-          element={
-              <PanelAdmin/>
-          }
-        />
-        // rutas del admin
-        <Route path="/admin/alumnos" element={<GestionUsuarios tipoUsuario="alumno" />} />
-        <Route path="/admin/profesores" element={<GestionUsuarios tipoUsuario="profesor" />} />
-        <Route path="/admin/reportes" element={<Reportes />} />
 
         <Route
-          path="panel/profesor"
+          path="/alumno/cursos"
           element={
-              <PanelProfesor/>
+            <RutaProtegida rolRequerido="alumno">
+              <Cursos />
+            </RutaProtegida>
           }
         />
-        // rutas del profesor
-        <Route path="/profesor/cursos" element={<CursosProfesor />} />
-        <Route path="/profesor/asignatura/:id" element={<AsignaturaDetalle />} />
-        <Route path="/profesor/:id/asistencia" element={<AsistenciaProfesor />} />
-        <Route path="/profesor/:id/notas" element={<NotasProfesor />} />
-        <Route path="/profesor/:id/anotaciones" element={<AnotacionesProfesor />} />
-        <Route path="/profesor/:id/evaluaciones" element={<EvaluacionesProfesor />} />
+
+        <Route
+          path="/alumno/notas"
+          element={
+            <RutaProtegida rolRequerido="alumno">
+              <Notas />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/alumno/asistencia"
+          element={
+            <RutaProtegida rolRequerido="alumno">
+              <Asistencia />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/alumno/anotaciones"
+          element={
+            <RutaProtegida rolRequerido="alumno">
+              <Anotaciones />
+            </RutaProtegida>
+          }
+        />
+
+        {/* ADMIN */}
+        <Route
+          path="/panel/admin"
+          element={
+            <RutaProtegida rolRequerido="admin">
+              <PanelAdmin />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/admin/alumnos"
+          element={
+            <RutaProtegida rolRequerido="admin">
+              <GestionUsuarios tipoUsuario="alumno" />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/admin/profesores"
+          element={
+            <RutaProtegida rolRequerido="admin">
+              <GestionUsuarios tipoUsuario="profesor" />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/admin/reportes"
+          element={
+            <RutaProtegida rolRequerido="admin">
+              <Reportes />
+            </RutaProtegida>
+          }
+        />
+
+        {/* PROFESOR */}
+        <Route
+          path="/panel/profesor"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <PanelProfesor />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/profesor/cursos"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <CursosProfesor />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/profesor/asignatura/:id"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <AsignaturaDetalle />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/profesor/:id/asistencia"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <AsistenciaProfesor />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/profesor/:id/notas"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <NotasProfesor />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/profesor/:id/anotaciones"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <AnotacionesProfesor />
+            </RutaProtegida>
+          }
+        />
+
+        <Route
+          path="/profesor/:id/evaluaciones"
+          element={
+            <RutaProtegida rolRequerido="profesor">
+              <EvaluacionesProfesor />
+            </RutaProtegida>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
